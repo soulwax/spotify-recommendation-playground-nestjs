2effdb8d5fd004632440d0b0eb116ea2f6be08eb
soulwax <soulwax-95@protonmail.com>
2025-11-12 18:33:34 +0100

    Integrate global configuration and Spotify module into the application
    - Added `ConfigModule` for environment variable management with validation using Joi.
    - Updated `AppModule` to include `SpotifyModule` and configured global access to environment variables.
    - Refactored `main.ts` to utilize `ConfigService` for dynamic port retrieval and logging application startup information.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,27 @@
 
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SpotifyModule } from './spotify/spotify.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        SPOTIFY_CLIENT_ID: Joi.string().required(),
+        SPOTIFY_CLIENT_SECRET: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string().default('development'),
+      }),
+    }),
+    HttpModule,
+    SpotifyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
